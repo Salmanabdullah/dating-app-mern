d436470a0d48dd18fb66fd760e3e012990fa4ba1
Salmanabdullah <salmandcc@gmail.com>
2023-10-05 18:17:44 +0200

    initial commit

---
+++ dating-app-frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Components/Header";
+import DatingCards from "./Components/DatingCards";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <DatingCards />
+    </div>
+  );
+}
+
+
+export default App;
