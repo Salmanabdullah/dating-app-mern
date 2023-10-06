d553a03b2769001a6ec151cebbbd782be7f67f8d
Salmanabdullah <salmandcc@gmail.com>
2023-10-06 18:40:04 +0200

    add swipe buttons

--- dating-app-frontend/src/App.js
+++ dating-app-frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
 import DatingCards from "./Components/DatingCards";
+import SwipeButtons from "./Components/SwipeButtons";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
